e79801da4a5671fdb374aa620d63f4287dee4d92
Rishikesh <krishikesh369@gmail.com>
2024-01-22 03:39:01 +0530

    few changes

--- routes/Categories.js
+++ routes/Categories.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { fetchCategories, createCategory } = require('../controller/Category');
+const { isAuth } = require('../services/common');
 
 const router = express.Router();
 //  /categories is already added in base path
-router.get('/', fetchCategories).post('/',createCategory)
+router.get('/', fetchCategories).post('/',isAuth() ,createCategory)
 
 exports.router = router;
